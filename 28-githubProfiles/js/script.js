9b096398152919bbc541110d860e260d55222c0e
Matt Klinger <mattrklinger@gmail.com>
2025-05-19 10:23:10 -0500

    Github Profile Cards - Github API - Axios

--- 28-githubProfiles/js/script.js
+++ 28-githubProfiles/js/script.js
@@ -1,9 +1,12 @@
+// root URL
 const APIURL = "https://api.github.com/users/";
 
+// creating variables for elements in HTML based on ID
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
+// getting user data and then repos data APIURL/username
 async function getUser(username) {
   try {
     const { data } = await axios(APIURL + username);
@@ -17,9 +20,10 @@ async function getUser(username) {
   }
 }
 
+// getting repo information  APIURL/username/repos
 async function getRepos(username) {
   try {
-    const { data } = await axios(APIURL + username + "/repos?sort=created");
+    const { data } = await axios(APIURL + username + "/repos?sort=created"); // sorts by most recent repos
 
     addReposToCard(data);
   } catch (err) {
@@ -30,6 +34,8 @@ async function getRepos(username) {
 function createUserCard(user) {
   const userID = user.name || user.login;
   const userBio = user.bio ? `<p>${user.bio}</p>` : "";
+
+  // creating user card HTML using data from User object in API
   const cardHTML = `
     <div class="card">
     <div>
@@ -64,17 +70,21 @@ function createErrorCard(msg) {
 function addReposToCard(repos) {
   const reposEl = document.getElementById("repos");
 
+  // looping through repos and creating repo elements
+  // gets first 5 repos with slice method
   repos.slice(0, 5).forEach((repo) => {
     const repoEl = document.createElement("a");
     repoEl.classList.add("repo");
     repoEl.href = repo.html_url;
-    repoEl.target = "_blank";
+    repoEl.target = "_blank"; // opens in new window
     repoEl.innerText = repo.name;
 
-    reposEl.appendChild(repoEl);
+    reposEl.appendChild(repoEl); // insert elements into DOM
   });
 }
 
+// prevent default behavior (submit to a file)
+// get user data using search input
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -83,6 +93,7 @@ form.addEventListener("submit", (e) => {
   if (user) {
     getUser(user);
 
+    // clear search input value after getting user
     search.value = "";
   }
 });
