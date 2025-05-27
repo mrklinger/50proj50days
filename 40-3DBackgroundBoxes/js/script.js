e433fb05dc6f99fce0f0e7f1b7f9aefb6133963a
Matt Klinger <mattrklinger@gmail.com>
2025-05-27 11:42:34 -0500

    3D Background Boxes - Rotating Minions GIF

--- 40-3DBackgroundBoxes/js/script.js
+++ 40-3DBackgroundBoxes/js/script.js
@@ -3,12 +3,15 @@ const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => boxesContainer.classList.toggle("big"));
 
+// creating 16 boxes with double For loop
 function createBoxes() {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       const box = document.createElement("div");
       box.classList.add("box");
+      // remember to only use camel case
       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+      //insert the divs into the container
       boxesContainer.appendChild(box);
     }
   }
