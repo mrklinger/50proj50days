983eda3d1f5df92967b9f12a61fc5839de849f8a
Matt Klinger <mattrklinger@gmail.com>
2025-05-27 13:27:01 -0500

    Verify Account - 6 Digit Input

--- 41-verifyAccount/js/script.js
+++ 41-verifyAccount/js/script.js
@@ -1,7 +1,12 @@
+//bringing in all the inputs - put into node list
 const codes = document.querySelectorAll(".code");
 
+//automatically focuses on first input when page loads
 codes[0].focus();
 
+// creating function to automatically move focus to next input or hit Backspace to move backwards
+// need setTimeout step to allow user to input value into first input
+// line 13 ... .value = "" - allows user to easily enter new input if they hit backspace - will clear out the input
 codes.forEach((code, idx) => {
   code.addEventListener("keydown", (e) => {
     if (e.key >= 0 && e.key <= 9) {
