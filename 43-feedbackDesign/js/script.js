4629b21047db3092398dfb026eb1ef9c48202071
Matt Klinger <mattrklinger@gmail.com>
2025-05-29 12:53:32 -0500

    Feedback UI - Smiley Faces

--- 43-feedbackDesign/js/script.js
+++ 43-feedbackDesign/js/script.js
@@ -4,13 +4,18 @@ const sendBtn = document.querySelector("#send");
 const panel = document.querySelector("#panel");
 let selectedRating = "Satisfied";
 
+// using Event bubbling instead of a "For Each"
+// e.target shows the element that you are clicking (can console log this to see it)
 ratingsContainer.addEventListener("click", (e) => {
   if (
+    // if parent of the selected item has rating class (so the box of the smiley face)
     e.target.parentNode.classList.contains("rating") &&
     e.target.nextElementSibling
   ) {
     removeActive();
+    // add active class to the parent node (the box)
     e.target.parentNode.classList.add("active");
+    // save rating text of selected smiley - the "small" element is a sibling to the smiley image
     selectedRating = e.target.nextElementSibling.innerHTML;
   } else if (
     e.target.parentNode.classList.contains("rating") &&
@@ -23,6 +28,7 @@ ratingsContainer.addEventListener("click", (e) => {
   }
 });
 
+// changes panel contents on the click of the button
 sendBtn.addEventListener("click", (e) => {
   panel.innerHTML = `
         <i class="fas fa-heart"></i>
@@ -33,6 +39,7 @@ sendBtn.addEventListener("click", (e) => {
     `;
 });
 
+// remove active class on all the smiley boxes
 function removeActive() {
   for (let i = 0; i < ratings.length; i++) {
     ratings[i].classList.remove("active");
