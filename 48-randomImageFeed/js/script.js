710739089ecf99329d991530a9f0180090500c57
Matt Klinger <mattrklinger@gmail.com>
2025-05-31 15:01:04 -0500

    Random Image Generator - 5x3 Grid

--- 48-randomImageFeed/js/script.js
+++ 48-randomImageFeed/js/script.js
@@ -1,17 +1,23 @@
 const container = document.querySelector(".container");
+// random unsplash URL no longer works
 const unsplashURL = "https://source.unsplash.com/random/";
 const rows = 5;
 
+// multiply by 3 because we want 3 images per row
 for (let i = 0; i < rows * 3; i++) {
   const img = document.createElement("img");
   img.src = `${unsplashURL}${getRandomSize()}`;
   container.appendChild(img);
 }
 
+// gets random combinations of 302x306 and such
 function getRandomSize() {
   return `${getRandomNr()}x${getRandomNr()}`;
 }
 
+// want number between 300-310
+// round down => Math.floor
+// Math.random gets random decimal
 function getRandomNr() {
   return Math.floor(Math.random() * 10) + 300;
 }
