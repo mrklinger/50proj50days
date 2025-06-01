095e87b9d6dc562431fda50fd4df0a64487964b2
Matt Klinger <mattrklinger@gmail.com>
2025-06-01 08:41:43 -0500

    Bug Catch Game - Timer, Random Position in DOM

--- 50-insectCatchGame/js/script.js
+++ 50-insectCatchGame/js/script.js
@@ -7,22 +7,36 @@ const scoreEl = document.getElementById("score");
 const message = document.getElementById("message");
 let seconds = 0;
 let score = 0;
+// this will be object with img source and alt
 let selected_insect = {};
 
+////// SCREEN 1 //////
+
+// change screen by clicking Start button
 start_btn.addEventListener("click", () => screens[0].classList.add("up"));
 
+////// SCREEN 2 //////
+
+// loop through Choose Insect Buttons
 choose_insect_btns.forEach((btn) => {
   btn.addEventListener("click", () => {
+    // getting img and attributes of selected insect
     const img = btn.querySelector("img");
     const src = img.getAttribute("src");
     const alt = img.getAttribute("alt");
+    // set selected insect with info selected
     selected_insect = { src, alt };
+    // change to screen 3
     screens[1].classList.add("up");
+    // create insect and put on random place on screen
     setTimeout(createInsect, 1000);
     startGame();
   });
 });
 
+// game timer
+// m = minutes, s = seconds
+// functions are styling the m and s with ":05" etc.
 function startGame() {
   setInterval(increaseTime, 1000);
 }
@@ -36,12 +50,16 @@ function increaseTime() {
   seconds++;
 }
 
+// Creating and inserting insect into DOM
 function createInsect() {
   const insect = document.createElement("div");
   insect.classList.add("insect");
+  // randomize location to place insect - destructure to get value of X and Y from getRandomLocation function
   const { x, y } = getRandomLocation();
   insect.style.top = `${y}px`;
   insect.style.left = `${x}px`;
+  // adding img and alt info to div element (creating insect)
+  // setting a random rotation to each new insect with inline style
   insect.innerHTML = `<img src="${selected_insect.src}" alt="${
     selected_insect.alt
   }" style="transform: rotate(${Math.random() * 360}deg)" />`;
@@ -52,13 +70,18 @@ function createInsect() {
 }
 
 function getRandomLocation() {
+  // window width and height
   const width = window.innerWidth;
   const height = window.innerHeight;
+  // making sure insect will be fully visible within the screen
   const x = Math.random() * (width - 200) + 100;
   const y = Math.random() * (height - 200) + 100;
   return { x, y };
 }
 
+// removes insect from DOM once clicked
+// this. adds class to selected insect
+// need to include remove() to remove element from DOM instead of just disappear off screen
 function catchInsect() {
   increaseScore();
   this.classList.add("caught");
@@ -66,11 +89,13 @@ function catchInsect() {
   addInsects();
 }
 
+// adds 2 two insects after insect is clicked
 function addInsects() {
   setTimeout(createInsect, 1000);
   setTimeout(createInsect, 1500);
 }
 
+// increasing Score and adding annoying message
 function increaseScore() {
   score++;
   if (score > 19) {
